b057e5b0d704d9e216ee9d50dfa20c01ec9e2972
Gordei Kuznetsov <66005299+Gordei-Kuznetsov@users.noreply.github.com>
2021-05-12 09:07:43 +1200

    Initial commit, created the project

---
+++ VivedyWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VivedyWebApp.Startup))]
+namespace VivedyWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
