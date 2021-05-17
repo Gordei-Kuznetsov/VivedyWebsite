295fa862c241636b6a4f15ccb21964434bdfe024
Campbell-Lythe-Brown <73807815+Campbell-Lythe-Brown@users.noreply.github.com>
2021-05-17 14:16:45 +1200

    Added AspNet Roles to Table

--- VivedyWebApp/Startup.cs
+++ VivedyWebApp/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Owin;
 using Owin;
 
+
 [assembly: OwinStartupAttribute(typeof(VivedyWebApp.Startup))]
 namespace VivedyWebApp
 {
@@ -9,6 +10,33 @@ namespace VivedyWebApp
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            //CreateRoles();
         }
+        /*private void CreateRoles()
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(_context));
+
+            // Used to create 2 Roles (Admin, Visitor)
+
+            
+             * if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+            }
+            
+
+            
+            if (!roleManager.RoleExists("Visitor"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Visitor";
+                roleManager.Create(role);
+            }
+            
+        }*/
     }
 }
