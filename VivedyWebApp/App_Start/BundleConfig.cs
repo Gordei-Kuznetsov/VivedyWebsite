5ed0415d0f12a5b65fee30255a7e6364806e2c2f
Gordei Kuznetsov <66005299+Gordei-Kuznetsov@users.noreply.github.com>
2021-05-21 22:30:17 +1200

    Added new layout page, fonts, boostrap

--- VivedyWebApp/App_Start/BundleConfig.cs
+++ VivedyWebApp/App_Start/BundleConfig.cs
@@ -20,10 +20,12 @@ namespace VivedyWebApp
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/navbarred.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/vivedy-bootstrap.css",
+                      "~/Content/all.min.css",
                       "~/Content/site.css"));
         }
     }
