c6b3eae497217c68ed0a06e9a9a7fe05b6140d82
Campbell-Lythe-Brown <73807815+Campbell-Lythe-Brown@users.noreply.github.com>
2021-06-08 15:47:02 +1200

    Completed Search and Filter Script

--- VivedyWebApp/Scripts/Search and Filter.js
+++ VivedyWebApp/Scripts/Search and Filter.js
@@ -1,20 +1,20 @@
 ﻿let movieSearch = document.getElementById("SearchFor");
 let categoryDropdown = document.getElementById("categoryFilter");
-let ratingDropdown = document.getElementById("ratingDropdown");
-let items = [];
-document.onload = () => {
+let ratingDropdown = document.getElementById("ratingFilter");
+let items = Array();
+window.onload = function () {
     let ul = document.getElementById("Movies");
     items = ul.getElementsByTagName("li");
-    items.forEach(item => {
-        item.unmatched = false;
-        item.filteredByRating = false;
-        item.filteredByCategory = false;
-        item.moviename = item.getElementsByClassName("moviename")[0].innerText;
-        item.category = item.getElementsByClassName("moviecategory")[0].innerText;
-        item.rating = item.getElementsByClassName("movierating")[0].innerText;
-    });
+    for (let i = 0; i < items.length; i++) {
+        items[i].unmatched = false;
+        items[i].filteredByRating = false;
+        items[i].filteredByCategory = false;
+        items[i].moviename = items[i].getElementsByClassName("moviename")[0].innerText;
+        items[i].category = items[i].getElementsByClassName("moviecategory")[0].innerText;
+        items[i].rating = items[i].getElementsByClassName("movierating")[0].innerText;
+    }
 }
-function SelectionChange() {
+function CategorySelectionChange() {
     var selected = [];
     for (var option of categoryDropdown.options) {
         if (option.selected) {
@@ -23,64 +23,75 @@ function SelectionChange() {
     }
     filterCategory(selected);
 }
+function RatingSelectionChange() {
+    var selected = [];
+    for (var option of ratingDropdown.options) {
+        if (option.selected) {
+            selected.push(option.value)
+        }
+    }
+    filterRating(selected);
+}
 function SearchForMovie() {
-    items.forEach(item => {
-        if (item.value.toUpperCase().indexOf(movieSearch.value.toUpperCase()) > -1) {
-            item.unmatched = false;
-            if (!item.filteredByRating && !item.filteredByCategory) {
-                item.style.display = "";
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].innerText.toUpperCase().indexOf(movieSearch.value.toUpperCase()) > -1) {
+            items[i].unmatched = false;
+            if (!items[i].filteredByRating && !items[i].filteredByCategory) {
+                items[i].style.display = "";
             }
             else {
-                item.style.display = "none";
+                items[i].style.display = "none";
             }
         }
         else {
-            item.unmatched = true;
-            item.style.display = "none";
+            items[i].unmatched = true;
+            items[i].style.display = "none";
         }
 
 
-    });
+    };
 }
 function filterCategory(categories) {
-    items.forEach(item => {
-        categories.forEach(category => {
-            if (item.category == category) {
-                if (!item.unmatched && !item.filteredByRating) {
-                    item.filteredByCategory = false
-                    item.style.display = "";
-                    return;
+    for (let j = 0; j < categories.length; j++) {
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].category == categories[j]) {
+                items[i].filteredByCategory = false
+                if (!items[i].unmatched && !items[i].filteredByRating) {
+                    items[i].style.display = "";
+                    continue;
                 }
                 else {
-                    item.style.display = "none"
+                    items[i].style.display = "none"
                 }
             }
-
-        });
-        item.filteredByCategory = true;
-        item.style.display = "none";
-    });
+            else {
+                items[i].filteredByCategory = true;
+                items[i].style.display = "none";
+            }
+        };
+    };
 }
 function filterRating(ratings) {
-    items.forEach(item => {
-        ratings.forEach(rating => {
-            if (item.rating == rating) {
-                if (!item.unmatched && item.filterCategory) {
-                    item.filteredByRating = false;
-                    item.style.display = "";
-                    return;
+    for (let j = 0; j < ratings.length; j++) {
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].rating == ratings[j]) {
+                items[i].filteredByRating = false;
+                if (!items[i].unmatched && !items[i].filteredByCategory) {
+                    items[i].style.display = "";
+                    continue;
                 }
                 else {
-                    item.style.display = "none"
+                    items[i].style.display = "none"
                 }
 
             }
-        });
-        item.filteredByRating = true;
-        item.style.display = "none";
-    });
+            else {
+                items[i].filteredByRating = true;
+                items[i].style.display = "none";
+            }
+        };
+    };
 }
-
 
 
 
