bd760bb3350a8ec8ede7f50f464e2bb212269896
Campbell-Lythe-Brown <73807815+Campbell-Lythe-Brown@users.noreply.github.com>
2021-06-22 13:16:28 +1200

    Added Cinemas Model

---
+++ VivedyWebApp/Models/Cinema.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VivedyWebApp.Models
+{
+    public class Cinema
+    {
+        [Display(Name = "Cinema Id")]
+        [Key]
+        [Required]
+        public string CinemaId { get; set; }
+
+        [Display(Name = "Cinema Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Cinema Address")]
+        [Required]
+        public string Address { get; set; }
+
+        [Display(Name = "Seats Layout")]
+        [Required]
+        public string SeatsLayout { get; set; }
+    }
+}
